06111021219ec39a908b8693d56831282c5ff35f
bmihetu <motta4259@gmail.com>
2019-07-16 16:34:35 -0400

    first commit

---
+++ src/main/java/com/example/demo/Cla.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cla {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cla.class, args);
+    }
+
+}
